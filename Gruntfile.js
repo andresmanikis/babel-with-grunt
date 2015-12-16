f5299e7109d6b1e40d38b90f8e56eb5a0900ef8c
Andrés Manikis <andresmanikis@gmail.com>
2015-12-15 21:04:11 -0300

    Adds babel and creates a simple no-transformation task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,26 @@
-/* global module */
-
 module.exports = function(grunt) {
   'use strict';
+
+  require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('basicTask', 'Log some stuff', function() {
     grunt.log.write('Logging some stuff...').ok();
     return true;
   });
 
-  grunt.registerTask('default', ['basicTask']);
+  grunt.initConfig({
+    babel: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/app.js': 'src/app.js'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['babel']);
 
 };
---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Person {
+
+  sayHi() {
+    console.log('Hi! I\'m a person!');
+  }
+
+}
+
+var person = new Person();
+person.sayHi();
